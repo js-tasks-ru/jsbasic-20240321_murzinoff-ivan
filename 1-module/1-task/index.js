84ad196df99467fbe6cfa0fcd362fcb6ea1b48ba
Ivan Murzinov <murzinoff.ivan@gmail.com>
2024-03-25 01:54:11 +0300

    fact func solution

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  if (n === 0) {
+    return result;
+  } else {
+    while (n > 0) {
+      result *= n;
+      n--;
+    }
+    return result;
+  }
 }
