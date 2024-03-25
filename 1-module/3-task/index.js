6f9c76609a15cbcac98258e7a157f1bd16720841
Ivan Murzinov <murzinoff.ivan@gmail.com>
2024-03-25 13:31:50 +0300

    firstCharCap func solution

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,7 @@
 function ucFirst(str) {
   // ваш код...
+  if (str === '') return str;
+  let firstLetter = str[0];
+  firstLetter = firstLetter.toUpperCase();
+  return firstLetter + str.substring(1);
 }
