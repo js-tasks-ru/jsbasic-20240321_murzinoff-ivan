c572b1ff4bee1a23e796654d7e7cbc7c48ccc019
Ivan Murzinov <murzinoff.ivan@gmail.com>
2024-03-25 15:47:13 +0300

    spam func

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
   // ваш код...
+
+  if (str.toLowerCase().indexOf('xxx') != -1 || str.toLowerCase().indexOf('1xbet') != -1) {
+    return true;
+    } else {
+      return false;
+  }
 }
