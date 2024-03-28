975cf89547c2608eedbe9d09860607b47fbcbf27
Ivan Murzinov <murzinoff.ivan@gmail.com>
2024-03-28 21:53:39 +0300

    lesson 2 HW

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,17 @@
-let calculator = {
-  // ваш код
-};
+const calculator = {
+  read(a, b) {
+    this.firstNum = a;
+    this.secondNum = b;
+  },
+  
+  sum() {
+    return this.firstNum + this.secondNum;
+  },
+  
+  mul() {
+    return this.firstNum * this.secondNum;
+  }
+}
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
