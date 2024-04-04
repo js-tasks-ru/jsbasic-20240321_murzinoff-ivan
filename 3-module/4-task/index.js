b6e7df365bf92747effc576d997ae75ae1732084
Ivan Murzinov <murzinoff.ivan@gmail.com>
2024-04-04 23:41:06 +0300

    add task 4/1 salaryList func

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
   // ваш код...
+  let salaryList = '';
+  for (let prop in users) {
+    if (users[prop].age <= age){
+      salaryList = salaryList + users[prop].name + ', ' + users[prop].balance + '\n';
+    }
+  }
+  return salaryList.slice(0, -1);
 }
