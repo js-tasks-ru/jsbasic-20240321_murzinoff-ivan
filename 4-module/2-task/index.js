e4c5e96f6cd2cc81c182533958fd400778f6c075
Ivan Murzinov <murzinoff.ivan@gmail.com>
2024-04-07 18:48:42 +0300

    add makeDiagonalRed func

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,10 @@
 function makeDiagonalRed(table) {
   // ваш код...
+
+  let i = 0;
+
+  while (table.rows[i] && table.rows[i].cells[i]) {
+    table.rows[i].cells[i].style.backgroundColor = 'red';
+    i++;
+  }
 }
