e838ca6246679c96759902dfce07005747d79ca0
Ivan Murzinov <murzinoff.ivan@gmail.com>
2024-04-07 16:14:22 +0300

    lesson 4 HW

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,16 @@
 function makeFriendsList(friends) {
   // ваш код...
+  const ul = document.createElement('ul');
+
+  for (let person of friends) {
+
+    const li = document.createElement('li');
+
+    li.textContent = person.firstName + person.lastName;
+
+    ul.append(li)
+  }
+
+  return ul;
+
 }
