1d1ffd47c9b37512f837447a76b85649233948d9
Ivan Murzinov <murzinoff.ivan@gmail.com>
2024-04-14 20:43:26 +0300

    task 2 module 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,15 @@
 function toggleText() {
   // ваш код...
+  const btn = document.querySelector('button');
+  const text = document.querySelector('div');
+
+  btn.addEventListener('click', function() {
+    if (text.dataset.hidden === 'no') {
+      text.hidden = 'true';
+      text.dataset.hidden = 'yes';
+    } else {
+      text.hidden = '';
+      text.dataset.hidden = 'no';
+    }
+  })
 }
