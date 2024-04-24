84cb9e5349a201a37afa14f9901a560547a409fa
Ivan Murzinov <murzinoff.ivan@gmail.com>
2024-04-24 18:42:51 +0300

    add module 6 task 1 class solution

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,61 @@
  *      }
  *
  */
+
+
+
 export default class UserTable {
+  elem = null;
+
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.#render();
+  }
+
+  #render() {
+    const table = document.createElement('table');
+    const head = document.createElement('thead');
+    const body = document.createElement('tbody');
+
+    const header = document.createElement('tr');
+    ['Имя', 'Возраст', 'Зарплата', 'Город', ''].forEach(columnName => {
+      const th = document.createElement('th');
+      th.textContent = columnName;
+      header.append(th);
+    });
+
+    head.append(header);
+
+
+    this.rows.forEach(obj => {
+      const row = document.createElement('tr');
+
+      for (let key in obj) {
+        const td = document.createElement('td');
+        td.textContent = obj[key];
+        row.append(td);
+      }
+
+      const deleteTd = document.createElement('td');
+      const deleteButton = document.createElement('button');
+
+      deleteButton.textContent = '[X]';
+      deleteButton.addEventListener('click', () => {
+        row.remove();
+      });
+
+      deleteTd.append(deleteButton);
+      row.append(deleteTd);
+      body.append(row)
+
+    })
+
+
+    table.append(head);
+    table.append(body);
+  
+    return table;
+
   }
 }
+
